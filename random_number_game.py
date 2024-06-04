b6b9c0e9856dbc6754670926b03095c712a8c8db
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:39:57 +0100

    Level5 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -2,13 +2,20 @@ import random
 
 random_number = random.randint(1,10)
 ALLOWED_VALUES = list(range(1,11))
-input_number = int(input("Input a number here: "))
-if input_number not in ALLOWED_VALUES:
-    raise ValueError(f"Input value must be {ALLOWED_VALUES}")
-if input_number == random_number:
-    print(f"Success! You guessed the random number was {input_number}!")
-elif abs(input_number - random_number) == 1:
+
+valid_input = False
+while not valid_input:
+    input_number = input("Input a number here: ")
+    if not input_number.isdigit() or (input_number.isdigit() and int(input_number) not in ALLOWED_VALUES):
+        print(f"Input value must be {ALLOWED_VALUES}")
+    else:
+        valid_input = True
+
+verified_input_number = int(input_number)
+if verified_input_number == random_number:
+    print(f"Success! You guessed the random number was {verified_input_number}!")
+elif abs(verified_input_number - random_number) == 1:
     print("Close - you were one away")
 else:
-    print(f"Fail! You chose {input_number} - the number to guess was {random_number}")
+    print(f"Fail! You chose {verified_input_number} - the number to guess was {random_number}")
 
