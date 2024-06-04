6e760f01cf12d97a93870047a0d2c8d6535bdfdd
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:29:52 +0100

    Level3 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -1,8 +1,10 @@
 import random
 
 random_number = random.randint(1,10)
-
+ALLOWED_VALUES = list(range(1,11))
 input_number = int(input("Input a number here: "))
+if input_number not in ALLOWED_VALUES:
+    raise ValueError(f"Input value must be {ALLOWED_VALUES}")
 if input_number == random_number:
     print(f"Success! You guessed the random number was {input_number}!")
 else:
