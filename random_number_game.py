a25b231f412888748d0425b71e2e4991984ed5d9
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:27:29 +0100

    Level1 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -2,8 +2,8 @@ import random
 
 random_number = random.randint(1,10)
 
-input_number = input("Input a number here: ")
-if input_number = random_number:
+input_number = int(input("Input a number here: "))
+if input_number == random_number:
     print("Success! You guessed the random number!")
 else:
     print(f"Fail! You chose {input_number} - the number to guess was {random_number}")
