253fde5990df904ea1534523f2bbcb361bc19d0f
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:52:42 +0100

    Level8 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -7,19 +7,22 @@ def magic_number():
     random_number = random.randint(1,10)
     guess_count = 0
     while guess_count < 3:
-        input_number = input("Input a number here: ")
-        if not input_number.isdigit() or (input_number.isdigit() and int(input_number) not in ALLOWED_VALUES):
-            print(f"Input value must be {ALLOWED_VALUES}")
-        else:
-            verified_input_number = int(input_number)
+        try:
+            input_number = int(input("Input a number here: "))
+            if input_number not in ALLOWED_VALUES:
+                raise ValueError("Not a valid input number")
             guess_count += 1
-            if verified_input_number == random_number:
-                print(f"Success! You guessed the random number was {verified_input_number}!")
+            if input_number == random_number:
+                print(f"Success! You guessed the random number was {input_number}!")
                 # Break the loop
                 break
-            elif abs(verified_input_number - random_number) == 1:
+            elif abs(input_number - random_number) == 1:
                 print("Close - you were one away")
             else:
-                print(f"Fail! You chose {verified_input_number} - the number to guess was {random_number}")
+                print(f"Fail! You chose {input_number} - the number to guess was {random_number}")
+
+        except ValueError:
+            print(f"Input value must be {ALLOWED_VALUES}")
+        
 
 magic_number()
