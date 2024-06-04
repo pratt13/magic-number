c7eeb3169ba51ad3b0113897a28336504e5d8efc
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:30:51 +0100

    Level4 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -7,6 +7,8 @@ if input_number not in ALLOWED_VALUES:
     raise ValueError(f"Input value must be {ALLOWED_VALUES}")
 if input_number == random_number:
     print(f"Success! You guessed the random number was {input_number}!")
+elif abs(input_number - random_number) == 1:
+    print("Close - you were one away")
 else:
     print(f"Fail! You chose {input_number} - the number to guess was {random_number}")
 
