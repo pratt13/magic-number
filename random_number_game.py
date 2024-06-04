18b729c953892936016ab884d91c0d5b5f570ec4
Pete Pratt <ppratt@caci.co.uk>
2024-06-04 20:47:42 +0100

    Level7 - complete

--- random_number_game.py
+++ random_number_game.py
@@ -3,20 +3,23 @@ import random
 random_number = random.randint(1,10)
 ALLOWED_VALUES = list(range(1,11))
 
-guess_count = 0
-while guess_count < 3:
-    input_number = input("Input a number here: ")
-    if not input_number.isdigit() or (input_number.isdigit() and int(input_number) not in ALLOWED_VALUES):
-        print(f"Input value must be {ALLOWED_VALUES}")
-    else:
-        verified_input_number = int(input_number)
-        guess_count += 1
-        if verified_input_number == random_number:
-            print(f"Success! You guessed the random number was {verified_input_number}!")
-            # Break the loop
-            break
-        elif abs(verified_input_number - random_number) == 1:
-            print("Close - you were one away")
+def magic_number():
+    random_number = random.randint(1,10)
+    guess_count = 0
+    while guess_count < 3:
+        input_number = input("Input a number here: ")
+        if not input_number.isdigit() or (input_number.isdigit() and int(input_number) not in ALLOWED_VALUES):
+            print(f"Input value must be {ALLOWED_VALUES}")
         else:
-            print(f"Fail! You chose {verified_input_number} - the number to guess was {random_number}")
+            verified_input_number = int(input_number)
+            guess_count += 1
+            if verified_input_number == random_number:
+                print(f"Success! You guessed the random number was {verified_input_number}!")
+                # Break the loop
+                break
+            elif abs(verified_input_number - random_number) == 1:
+                print("Close - you were one away")
+            else:
+                print(f"Fail! You chose {verified_input_number} - the number to guess was {random_number}")
 
+magic_number()
